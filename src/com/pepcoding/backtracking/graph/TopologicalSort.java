49da1b7765a8cce1186b9d6d82e2b2548d22add3
sauravdas90 <saurav.d336@gmail.com>
2021-09-16 00:12:05 +0530

    recursive topological sort

---
+++ src/com/pepcoding/backtracking/graph/TopologicalSort.java
@@ -0,0 +1,68 @@
+package com.pepcoding.backtracking.graph;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class TopologicalSort {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int nodes = in.nextInt();
+        int edges = in.nextInt();
+
+        boolean visited[] = new boolean[nodes];
+        LinkedList<NodeWeight>[] graph = new LinkedList[nodes];
+        Stack<Integer> sortedNode = new Stack<>();
+
+        for(int i =0; i< nodes; i++){
+            graph[i] = new LinkedList<>();
+        }
+
+        for(int i =0; i< edges; i++){
+            int u = in.nextInt();
+            int v = in.nextInt();
+            int w = in.nextInt();
+            // directed only graph
+            graph[u-1].add(new NodeWeight(v,w));
+        }
+
+
+        // loop through the nodes
+        for (int i = 0; i < nodes; i++) {
+            if(!visited[i]) {
+                // only visited the nodes, if not visited
+                // making it only i+1, as array indexing
+                doTopologicalSort(graph, visited, i + 1, sortedNode);
+            }
+        }
+        while(! sortedNode.isEmpty()){
+            System.out.print(" node = " + sortedNode.pop() + "\t");
+        }
+    }
+
+    public static void doTopologicalSort(LinkedList<NodeWeight>[] graph, boolean visited[], int src, Stack<Integer> sortedNode ){
+
+        // checking if the outdegree, like how many nodes are dependent
+        int outdegree = graph[src-1].size();
+        if(outdegree == 0 ){
+            // adding the nodes with no dependency
+            if (!visited[src-1]) {
+                sortedNode.push(src);
+                // marking the node as visited
+                visited[src-1] = true;
+            }
+            // as it has no outdegree so return back
+            return;
+        }
+
+        for(NodeWeight nbr: graph[src-1]){
+            if (!visited[nbr.vertex-1]) {
+                doTopologicalSort(graph,visited, nbr.vertex, sortedNode);
+            }
+        }
+        // marking the node as visited
+        visited[src-1] = true;
+        sortedNode.push(src);
+    }
+}
